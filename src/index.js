506ecffcaaf4dee1d93e154a60e61c0c63d96016
william <alown666@gmail.com>
2021-06-17 01:45:43 +0200

    console.log

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
-
+  console.log("before prompt event was fired")
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
