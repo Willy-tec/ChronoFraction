86dd4d139327b8c0bc900a9e90d10db36e000dd9
william <alown666@gmail.com>
2021-06-17 02:49:26 +0200

    Ok

--- src/sw.js
+++ src/sw.js
@@ -4,27 +4,29 @@ const CACHE_NAME = "offline"
 self.addEventListener('install', (e) => {
     console.log(caches.open("cache"))
     console.log('/ChronoFraction/index.html')
-    e.waitUntil(
-/*       caches.open("cache").then((cache) =>{return cache.addAll([
+    e.waitUntil(()=>{
+      caches.open("cache").then((cache) =>{
+        return cache.addAll([
+          '/index.html',
+          '/src/index.js',
+          '/src/style.css',
+          '/src/BEEP.mp3',
+          '/src/Chronometre.css',
+          '/src/timer.png',
+          '/src/BEEP.mp3'
+          ]).catch((e)=> console.log(e))
+        })
+      })
 
-        'index.html',
-        'src/index.js',
-        'src/style.css',
-        'src/BEEP.mp3',
-        'src/Chronometre.css',
-        'src/timer.png',
-        'src/BEEP.mp3'
 
-      ]).catch((e)=> console.log(e)) */
-
-    //}),
-    (async () => {
+    
+/*     (async () => {
       const cache = await caches.open(CACHE_NAME);
       // Setting {cache: 'reload'} in the new request will ensure that the response
       // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
       await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
     })()
-    );
+    ); */
   });
 
 self.addEventListener('fetch', (e) => {
